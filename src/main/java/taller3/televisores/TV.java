6bb6095f1ad5fa95e7ab2ab1d2e66d9871901cde
juanjoseArangom <jarangoma@unal.edu.co>
2024-02-25 13:19:45 -0500

    cambio en canal

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -49,7 +49,9 @@ public class TV {
     }
 
     public void setCanal(int canal){
-        this.canal = canal;
+        if (estado == true && canal >= 1 && canal <= 120) {
+            this.canal = canal;
+        }
     }
 
     public int getCanal(){
